f6aec0240acd23e7cc72958790a5b63dd4e3a11e
Olga Dokukova <dokukova@gmail.com>
2019-12-16 00:51:08 +0100

    [DOC] change readme

--- src/beers/screens/single-beer.tsx
+++ src/beers/screens/single-beer.tsx
@@ -16,7 +16,6 @@ const SingleBeer: React.FC<MyProps> = ({ match }) => {
 
 	const dispatch = useDispatch();
 	const beers = useSelector((state: any) => state.beersReducer.beers);
-	const currentPage = useSelector((state: any) => state.beersReducer.currentPage);
 
 	useEffect(() => {
 		if (!beers.length) {
